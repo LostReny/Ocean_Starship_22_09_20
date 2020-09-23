2d2903822b1b24958dc0d8d87de5d2f2153aa761
LostReny <68451719+LostReny@users.noreply.github.com>
2020-09-23 19:31:46 -0300

    Adicionado Movimento da Nave

---
+++ Assets/Scripts/Ship.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ship : MonoBehaviour
+{
+    public float speed = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        Vector3 position = transform.position;
+        position.x += x * Time.deltaTime * speed;
+        position.y += y * Time.deltaTime * speed;
+        transform.position = position;
+    }
+}
